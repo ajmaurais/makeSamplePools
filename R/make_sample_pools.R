4ed588a79f867534b30a98fa1a163f5f1c74a962
Aaron Maurais <41207472+ajmaurais@users.noreply.github.com>
2021-04-29 00:35:39 -0400

    Update make_sample_pools.R

--- R/make_sample_pools.R
+++ R/make_sample_pools.R
@@ -17,8 +17,7 @@ dat$bin <- binValues(floor(nrow(dat)/POOL_SIZE), nrow(dat))
 
 #create groups
 dat$group <- -1
-for(bin in unique(dat$bin))
-{
+for(bin in unique(dat$bin)) {
   dat[dat$bin == bin,]$group <- c(1:nrow(dat[dat$bin == bin,]))
 }
 
@@ -28,8 +27,8 @@ summary <- dat %>% dplyr::group_by(group) %>%
 
 output <- dat
 output <- output[order(output$group),]
-write.table(output, file = 'results/compound_groups.tsv', sep = '\t', row.names = FALSE, quote = F)
-write.table(summary, file = 'results/groups_summary.tsv', sep = '\t', row.names = FALSE, quote = F)
+write.table(output, file = 'results/compound_groups.tsv', sep = '\t', row.names = F, quote = F)
+write.table(summary, file = 'results/groups_summary.tsv', sep = '\t', row.names = F, quote = F)
 
 output2 <- output %>% dplyr::select(-mw) %>%
   tidyr::spread(key = group, value = ID)
